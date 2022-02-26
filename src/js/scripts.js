5f59656c34c204427397080077c1f7b5b1c7f92d
OscarJVD <oscarjaviervargas@hotmail.com>
2022-02-26 01:51:25 -0500

    arc webgl

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
-// import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js'
-// import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-// const monkeyUrl = new URL('./arc_glb.glb', import.meta.url)
+const monkeyUrl = new URL('../../assets/arc_glb.glb', import.meta.url)
 
-// console.log(monkeyUrl)
+console.log(monkeyUrl)
 
 const renderer = new THREE.WebGLRenderer()
 
@@ -21,4 +21,44 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 
-renderer.render(scene, camera)
+renderer.setClearColor(0xE1A3A1)
+
+const orbit = new OrbitControls(camera, renderer.domElement)
+
+camera.position.set(2, 2, 1)
+orbit.update();
+
+const grid = new THREE.GridHelper(20, 20)
+scene.add(grid)
+
+const assetLoader = new GLTFLoader()
+
+let mixer;
+
+assetLoader.load(monkeyUrl.href, function (gltf) {
+  const model = gltf.scene
+  scene.add(model)
+  mixer = new THREE.AnimationMixer(model)
+  const clips = gltf.animations;
+  console.log('clips', clips)
+  const clip = THREE.AnimationClip.findByName(clips, 'Armature')
+  const action = mixer.clipAction(clip)
+  action.play()
+}, undefined, function (err) {
+  console.error(err)
+})
+
+const clock = new THREE.Clock()
+function animate() {
+  if (mixer)
+    mixer.update(clock.getDelta())
+  renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
